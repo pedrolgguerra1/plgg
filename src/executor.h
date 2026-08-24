81236aeb9bb8ff927699100f58225d3370e55b49
Pedro <pguerrape@gmail.com>
2026-08-24 20:23:02 -0300

    adicionando nova assinatura

--- src/executor.h
+++ src/executor.h
@@ -4,7 +4,7 @@
 #include "../processflow.h"
 
 int executor_run_single(Task *t);
-
 void executor_run_sequential(int count, char *names[]);
+void executor_run_parallel(int count, char *names[]);
 
 #endif
