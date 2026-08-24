e08537a9f6662e2f2a2457a01c5445bf8d7a5bf7
Pedro <pguerrape@gmail.com>
2026-08-24 20:29:33 -0300

    nova assinatura

--- src/executor.h
+++ src/executor.h
@@ -6,5 +6,6 @@
 int executor_run_single(Task *t);
 void executor_run_sequential(int count, char *names[]);
 void executor_run_parallel(int count, char *names[]);
+void executor_run_pipe(int count, char *names[]);
 
 #endif
